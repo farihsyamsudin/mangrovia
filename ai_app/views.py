8da42a8dc48fdbac416d75a73127ff2515c0ffdf
farihsyamsudin <farihsyamsudin31@upi.edu>
2023-12-31 14:53:30 +0700

    benerin req, sama matiin fitur

--- ai_app/views.py
+++ ai_app/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from joblib import load
 import pandas as pd
-from roboflow import Roboflow
+# from roboflow import Roboflow
 model, tree_rules, accuracy = load('./model/modelMangrovia.sav')
-from django.core.files.storage import default_storage
-import os
-from django.conf import settings
+# from django.core.files.storage import default_storage
+# import os
+# from django.conf import settings
 
 # Create your views here.
 
@@ -45,36 +45,36 @@ def predict_mangrove(request):
         }
         return render(request, 'predict.html', data)
 
-def predict_mangrove_image(request):
+# def predict_mangrove_image(request):
 
-    if request.method == "POST" :
-        gambar_mangrove = request.FILES['gambar_mangrove']
-        gambar_mangrove.name = 'image_to_predict.jpg'
-        default_storage.save(gambar_mangrove.name, gambar_mangrove)
+#     if request.method == "POST" :
+#         gambar_mangrove = request.FILES['gambar_mangrove']
+#         gambar_mangrove.name = 'image_to_predict.jpg'
+#         default_storage.save(gambar_mangrove.name, gambar_mangrove)
 
-        path_gambar = os.path.join(settings.MEDIA_ROOT, gambar_mangrove.name)
+#         path_gambar = os.path.join(settings.MEDIA_ROOT, gambar_mangrove.name)
 
-        rf = Roboflow(api_key="1lj3LwBCdqf2TPysCarW")
-        project = rf.workspace().project("jenis-mangrove")
-        model = project.version(1).model
+#         rf = Roboflow(api_key="1lj3LwBCdqf2TPysCarW")
+#         project = rf.workspace().project("jenis-mangrove")
+#         model = project.version(1).model
 
-        # infer on a local image
-        prediction = model.predict(path_gambar, confidence=40, overlap=30).json()
+#         # infer on a local image
+#         prediction = model.predict(path_gambar, confidence=40, overlap=30).json()
 
-        print(prediction)
+#         print(prediction)
 
-        data = {
-            "heading" : "Prediksi Jenis Mangrove",
-            "prediction" : prediction
-        }
+#         data = {
+#             "heading" : "Prediksi Jenis Mangrove",
+#             "prediction" : prediction
+#         }
 
-        os.remove(path_gambar)
+#         os.remove(path_gambar)
 
-        return render(request, 'image_predict.html', data)
+#         return render(request, 'image_predict.html', data)
     
-    else :
-        data = {
-            "heading" : "Prediksi Jenis Mangrove"
-        }
+#     else :
+#         data = {
+#             "heading" : "Prediksi Jenis Mangrove"
+#         }
 
-        return render(request, 'image_predict.html', data)
+#         return render(request, 'image_predict.html', data)
